8865dda7e48bd12090a515a3217c61a8f169f80c
mj806 <mj8064835@gmail.com>
2022-10-27 12:52:04 +0100

    moved src stuff into AniMySite folder

--- AniMySite Folder/src/RoutingPages.js
+++ AniMySite Folder/src/RoutingPages.js
@@ -1,8 +1,8 @@
 import React from "react";
 import { Routes, Route, useLocation } from "react-router-dom";
-import Home from "./Components/Pages/HomePage/Home";
-import Anime from "./Components/Pages/AnimePage/Anime";
-import Manga from "./Components/Pages/MangaPage/Manga";
+import Home from "../../src/Components/Pages/HomePage/Home";
+import Anime from "../../src/Components/Pages/AnimePage/Anime";
+import Manga from "../../src/Components/Pages/MangaPage/Manga";
 import Error from "./Error";
 import { AnimatePresence } from "framer-motion";
 
